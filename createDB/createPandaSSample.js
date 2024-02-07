f57884ad30d3aadf76d6c621ca35b6f6b4a62923
Paola Medina <medinaypaola@gmail.com>
2024-02-06 21:37:38 -0800

    Updated createPandaSSample

--- createDB/createPandaSSample.js
+++ createDB/createPandaSSample.js
@@ -1,4 +1,4 @@
-db = db.getSiblingDB('toDoSample')
+db = db.getSiblingDB('PandaSample')
 
 db.createCollection('shoes')
 shoeCollection = db.getCollection("shoes")
@@ -7,33 +7,36 @@ shoeCollection.insert(
 {
 	shoeName: "KungPao Shoe",
 	shoeDescription: "The shoe that will let you destress.",
-	shoeId: 976-2222,
+	shoeId: "976-2222",
 	shoeSize: 5.5,
 	shoeRating: 4.5,
-	sellerId: 3599,
-	shoePrice: 39.99
+	sellerId: "3599",
+	shoePrice: 39.99,
+	storeID: "80299"
 }
 )
 shoeCollection.insert(
 {
 	shoeName: "Geo shoe",
 	shoeDescription: "A shoe that prevents",
-	shoeId: 1,
+	shoeId: "1",
 	shoeSize: 5.5,
 	shoeRating: 4.5,
-	sellerId: 3599,
-	shoePrice: 39.99
+	sellerId: "3599",
+	shoePrice: 39.99,
+	storeID: "80299"
 }
 )
 shoeCollection.insert(
 {
 	shoeName: "Leap Shoe",
 	shoeDescription: "A shoe that is easy to jump",
-	shoeId: 1,
+	shoeId: "1",
 	shoeSize: 5.5,
 	shoeRating: 4.5,
-	sellerId: 3599,
-	shoePrice: 39.99
+	sellerId: "3599",
+	shoePrice: 39.99,
+	storeID: "80299"
 }
 )
 
@@ -43,35 +46,39 @@ buyerCollection.remove({})
 buyerCollection.insert(
 {
 	buyerName: "Robert Widjaja",
-	buyerID: 1111,
+	buyerID: "1111",
 	buyerEmail: "gameislife@outlook.com",
 	buyerPassword: "jddsajfafddl111123213",
-	subscriptionID: 0,
+	subscriptionID: "0",
 	shippingAddr: "some random streets some random city some random state 98001",
-	orderHistory: []
+	orderHistory: [],
+	cart: []
 }
 )
+
 buyerCollection.insert(
 {
-	buyerName: "Ving Leap Chen",
-	buyerID: 1111,
+	buyerName: "Veng Leap Chen",
+	buyerID: "1112",
 	buyerEmail: "leapwantsdopeshoe@gmail.com.",
 	buyerPassword: "jddsajfafddl111123213",
-	subscriptionID: 0,
+	subscriptionID: "0'",
 	shippingAddr: "some random streets some random city some random state 98001",
-	orderHistory: []
+	orderHistory: [],
+	cart: []
 }
 )
 
 buyerCollection.insert(
 	{
 		  buyerName: "Juan Gonzalas",
-		  buyerID: 1111,
+		  buyerID: "1113",
 		  buyerEmail: "forthepeople@icloud.com",
 		  buyerPassword: "jddsajfafddl111123213",
-		  subscriptionID: 0,
+		  subscriptionID: "0",
 		  shippingAddr: "some random streets some random city some random state 98001",
-		  orderHistory: []
+		  orderHistory: [],
+			cart: []
 	}
 	)
 	
@@ -83,40 +90,75 @@ sellerCollection.remove({})
 sellerCollection.insert(
 {
 	sellerName: "Canada Goose",
-	sellerID: 1111,
+	sellerID: "0",
 	sellerEmail: "letsgoose@goose.org",
 	sellerPassword: "jddsajfafddl111123213",
-	subscriptionID: 0,
-	storeID: 181818,
+	subscriptionID: "3",
+	storeID: "181818",
 	invList: [],
 	salesHistory: []
 
 }
 )
+
 sellerCollection.insert(
 {
 	sellerName: "Apple",
-	sellerID: 1111,
+	sellerID: "1",
 	sellerEmail: "apple@icloud.com",
 	sellerPassword: "jddsajfafddl111123213",
-	subscriptionID: 0,
-	storeID: 181818,
+	subscriptionID: "3",
+	storeID: "181819",
 	invList: [],
 	salesHistory: []
 
 }
 )
+
 sellerCollection.insert(
 {
 	sellerName: "Laurentia's factory",
-	sellerID: 1111,
+	sellerID: "2",
 	sellerEmail: "carmel@gmail.com",
 	sellerPassword: "jddsajfafddl111123213",
-	subscriptionID: 0,
-	storeID: 181818,
+	subscriptionID: "3",
+	storeID: "181820",
 	invList: [],
 	salesHistory: []
 
 }
 )
 
+
+db.createCollection('storefront')
+sellerCollection = db.getCollection("storefront")
+sellerCollection.remove({})
+sellerCollection.insert(
+{
+	sellerID: "0",
+	storeID: "181818",
+	invList: [],
+	salesHistory: [],
+	storePic: "canadaPic.png"
+}
+)
+
+sellerCollection.insert(
+	{
+		sellerID: "1",
+		storeID: "181819",
+		invList: [],
+		salesHistory: [],
+		storePic: "appleLogo.png"
+	}
+	)
+
+sellerCollection.insert(
+	{
+		sellerID: "2",
+		storeID: "181820",
+		invList: [],
+		salesHistory: [],
+		storePic: "lineWork.png"
+	}
+	)
