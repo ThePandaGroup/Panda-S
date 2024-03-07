69a92443d769e33025e04f528e35494130cf1d82
Paola Medina <medinaypaola@gmail.com>
2024-03-07 13:55:03 -0800

    modified .env and AppServer.ts to connect to mongo atlas

--- AppServer.ts
+++ AppServer.ts
@@ -6,7 +6,8 @@ dotenv.config();
 const port = process.env.PORT;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-const mongoDBConnection = 'mongodb://' + dbUser + ':' + encodeURIComponent(dbPassword) + process.env.DB_INFO;
+const dbProtocol = process.env.DB_PROTOCOL;
+const mongoDBConnection = dbProtocol + dbUser + ':' + encodeURIComponent(dbPassword) + process.env.DB_INFO;
 console.log("server db connection URL " + mongoDBConnection);
 
 let server: any = new App(mongoDBConnection).expressApp;
