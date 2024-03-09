5b21261d2f1aba217767b6f35fb915256ec58e39
Sergio Satyabrata <sergio.satyabrata@gmail.com>
2024-03-08 22:39:54 -0800

    added new dist dir

--- dist/panda-s/server/main.server.mjs
+++ dist/panda-s/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-UIZP6UKA.mjs";import"./chunk-FJM5RU6C.mjs";import"./chunk-C5WUQGZ6.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-IZUUPSF4.mjs";import"./chunk-FJM5RU6C.mjs";import"./chunk-C5WUQGZ6.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
