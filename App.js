75c24533b2d8688907081ee0a32d4e0323f263bf
Sergio Satyabrata <sergio.satyabrata@gmail.com>
2024-03-09 12:56:31 -0800

    implementing log out logic

--- App.js
+++ App.js
@@ -91,9 +91,13 @@ class App {
             }
         }));
         router.get('/app/logout', (req, res) => {
-            req.logout();
-            console.log("logging out");
-            res.redirect('/#/');
+            req.user = null;
+            req.session.destroy(err => {
+                if (err) {
+                    console.log('Error : Failed to destroy the session during logout.', err);
+                }
+                res.redirect('/#/');
+            });
         });
         // SHOES ROUTES
         // Query All Shoes
--- App.ts
+++ App.ts
@@ -129,9 +129,13 @@ class App {
 
 
     router.get('/app/logout', (req: RequestWithUser, res) => {
-      req.logout();
-      console.log("logging out");
-      res.redirect('/#/');
+      req.user = null;
+      req.session.destroy(err => {
+        if (err) {
+          console.log('Error : Failed to destroy the session during logout.', err);
+        }
+        res.redirect('/#/');
+      });
     });
 
 
