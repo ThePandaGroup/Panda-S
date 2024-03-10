15a72969e3d3c6efe3d136bc1824e8a747c2fcb6
Sergio Satyabrata <sergio.satyabrata@gmail.com>
2024-03-09 16:56:30 -0800

    added remove from cart

--- dist/panda-s/server/main.server.mjs
+++ dist/panda-s/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CX2E4ULV.mjs";import"./chunk-FJM5RU6C.mjs";import"./chunk-C5WUQGZ6.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-GLNIMZV7.mjs";import"./chunk-FJM5RU6C.mjs";import"./chunk-C5WUQGZ6.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
