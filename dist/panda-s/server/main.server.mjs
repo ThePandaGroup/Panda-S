4170c8d6524fbc146707d2522b6bbeca175d166d
Paola Medina <medinaypaola@gmail.com>
2024-03-11 01:58:02 -0700

    changed button sizes on nav bar

--- dist/panda-s/server/main.server.mjs
+++ dist/panda-s/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KUGGFYOL.mjs";import"./chunk-RHWHI25C.mjs";import"./chunk-3CK3APNH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-J727F53E.mjs";import"./chunk-RHWHI25C.mjs";import"./chunk-3CK3APNH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
