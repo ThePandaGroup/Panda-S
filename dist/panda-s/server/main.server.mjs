114567cb89850b6f9d9c2dbc80d9dad3ea71fb18
Paola Medina <medinaypaola@gmail.com>
2024-03-10 19:57:44 -0700

    added logo pictures and descriptions for storefronts

--- dist/panda-s/server/main.server.mjs
+++ dist/panda-s/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7YLE35D3.mjs";import"./chunk-RHWHI25C.mjs";import"./chunk-3CK3APNH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-7KSQEE63.mjs";import"./chunk-RHWHI25C.mjs";import"./chunk-3CK3APNH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
